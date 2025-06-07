905388ea5f5255739577e5d341a94f24c10e1576
cucumber229 <mavlav2005@gmail.com>
2025-06-07 05:33:16 +0300

    123

--- demo/src/main/resources/static/js/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggler = document.querySelector('.navbar-toggler');
-  const menu = document.getElementById('navbarMenu');
-  if (toggler && menu) {
-    toggler.addEventListener('click', () => {
-      menu.classList.toggle('show');
-    });
-  }
-});
